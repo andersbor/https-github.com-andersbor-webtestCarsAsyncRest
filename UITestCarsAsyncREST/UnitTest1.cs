723eca2d347b2b532ee1868e46a866c47bd11d48
Anders Børjesson <anbo@zealand.dk>
2019-10-05 10:58:05 +0200

    waiting for async REST

--- UITestCarsAsyncREST/UnitTest1.cs
+++ UITestCarsAsyncREST/UnitTest1.cs
@@ -1,13 +1,49 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace UITestCarsAsyncREST
 {
     [TestClass]
     public class UnitTest1
     {
+        private static readonly string DriverDirectory = "C:\\seleniumDrivers2";
+        private static IWebDriver _driver;
+
+        // https://www.automatetheplanet.com/mstest-cheat-sheet/
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _driver = new ChromeDriver(DriverDirectory); // fast
+            // if your Chrome browser was updated, you must update the driver as well ...
+            //    https://chromedriver.chromium.org/downloads
+            //_driver = new FirefoxDriver(DriverDirectory);  // slow
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            _driver.Navigate().GoToUrl("http://localhost:3000/");
+            string title = _driver.Title;
+            Assert.AreEqual("Car Shop", title);
+
+            IWebElement buttonElement = _driver.FindElement(By.Id("getAllButton"));
+            buttonElement.Click();
+
+            //IWebElement carList = _driver.FindElement(By.Id("carlist")); // No such element
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // decorator pattern?
+            IWebElement carList = wait.Until<IWebElement>(d => d.FindElement(By.Id("carlist")));
+            Assert.IsTrue(carList.Text.Contains("Volvo"));
         }
     }
 }
