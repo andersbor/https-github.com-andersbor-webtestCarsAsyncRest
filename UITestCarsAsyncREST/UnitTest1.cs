a74b9d9314f308175794e5d2e4e9270152d55a7a
Anders Børjesson <anbo@zealand.dk>
2021-11-03 19:57:16 +0100

    more precise tests

--- UITestCarsAsyncREST/UnitTest1.cs
+++ UITestCarsAsyncREST/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -20,10 +22,10 @@ namespace UITestCarsAsyncREST
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            // _driver = new ChromeDriver(DriverDirectory); // fast
+             _driver = new ChromeDriver(DriverDirectory); // fast
             // if your Chrome browser was updated, you must update the driver as well ...
             //    https://chromedriver.chromium.org/downloads
-            _driver = new FirefoxDriver(DriverDirectory);  // slow
+            //_driver = new FirefoxDriver(DriverDirectory);  // slow
         }
 
         [ClassCleanup]
@@ -35,7 +37,7 @@ namespace UITestCarsAsyncREST
         [TestMethod]
         public void TestMethod1()
         {
-            string url = "http://localhost:3000/";
+            //string url = "http://localhost:3000/";
             _driver.Navigate().GoToUrl("file:///C:/andersb/javascript/carsVue3/index.htm");
 
             string title = _driver.Title;
@@ -46,9 +48,20 @@ namespace UITestCarsAsyncREST
 
             //IWebElement carList = _driver.FindElement(By.Id("carlist")); // No such element
 
+            // pause(60) // NOT good, will always wait full 60 sec
+
             WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // decorator pattern?
             IWebElement carList = wait.Until(d => d.FindElement(By.Id("carlist")));
             Assert.IsTrue(carList.Text.Contains("Volvo"));
+
+            // We already did the waiting in the previous lines, so now we can go back to using the ordinary driver
+            ReadOnlyCollection<IWebElement> listElements = _driver.FindElements(By.TagName("li"));
+            Assert.AreEqual(3, listElements.Count);
+
+            Assert.IsTrue(listElements[0].Text.Contains("Volvo"));
+
+            // XPath, an advanced option to use By.XPath(...)
+            // https://www.guru99.com/handling-dynamic-selenium-webdriver.html
         }
     }
 }
